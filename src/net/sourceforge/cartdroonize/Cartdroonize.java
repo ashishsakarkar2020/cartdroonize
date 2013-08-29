b1dd28096ac05337e3c4b84f0837ff6eb8f1b30c
jascha <jaschacasadio@gmail.com>
2013-08-29 23:05:23 +0200

    Added copyBitmap()

--- src/net/sourceforge/cartdroonize/Cartdroonize.java
+++ src/net/sourceforge/cartdroonize/Cartdroonize.java
@@ -38,9 +38,9 @@ public class Cartdroonize {
 		 * @param in_image_input: The mutable input Bitmap.
 		 * @param in_image_max_size: The max size of the image in the ImageView.
 		 */
-		this.image_input = in_image_input;
-		this.image_output = Bitmap.createBitmap(this.image_input);
-		this.image_max_size = in_image_max_size;
+		image_input = in_image_input;
+		image_output = copyBitmap(image_input);
+		image_max_size = in_image_max_size;
 	}
 	
 	public void blackAndWhite(){
@@ -49,9 +49,9 @@ public class Cartdroonize {
 		 * @author Jascha Casadio 
 		 * @version 0.20130825
 		 */
-		for(int i = 0; i < this.image_input.getWidth(); i++){
-			for(int j = 0;j < this.image_input.getHeight(); j++){  
-				int pixel = this.image_input.getPixel(i, j);
+		for(int i = 0; i < image_input.getWidth(); i++){
+			for(int j = 0;j < .image_input.getHeight(); j++){  
+				int pixel = image_input.getPixel(i, j);
 				int red_current = Color.red(pixel);
 				int green_current = Color.green(pixel);
 				int blue_current = Color.blue(pixel);
@@ -68,8 +68,8 @@ public class Cartdroonize {
 		 * Clears a Bitmap setting all its pixels to (255,255,255).
 		 * @author Jascha Casadio
 		 * @version 0.20130823
-		 * @image: The mutable Bitmap to clear.
-		 * @return image: The cleared mutable Bitmap.
+		 * @input: The mutable Bitmap to clear.
+		 * @return The cleared mutable Bitmap.
 		 */
 		for(int i = 0; i < image.getWidth(); i++){
 			for(int j = 0; j < image.getHeight(); j++){
@@ -77,6 +77,17 @@ public class Cartdroonize {
 				}
 			}
 		return image;
+	}
+	
+	private Bitmap copyBitmap(Bitmap input){
+		/**
+		 * Returns a mutable copy of the input Bitmap.
+		 * @author Jascha Casadio
+		 * @versin 0.20130829
+		 * @input: The mutable Bitmap to copy.
+		 * @return A mutable copy of the input Bitmap.
+		 */
+		return input.copy(input.getConfig(), true);
 	}
 	
 	private void edgeConvolution(int threshold, int high){
@@ -106,131 +117,131 @@ public class Cartdroonize {
 		/* clear output image array */
 		//this.image_input = clearImage(this.image_input);
 		
-		for(int i=1; i<this.image_input.getWidth()-1; i++){
-			for(int j=1; j<this.image_input.getHeight()-1; j++){
+		for(int i=1; i < image_input.getWidth()-1; i++){
+			for(int j=1; j < image_input.getHeight()-1; j++){
 		
 				/* 0 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_0[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_0[a+1][b+1];
 					}
 				}
 		
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 		
 				/* 1 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_1[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_1[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 2 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_2[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_2[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 3 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_3[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_3[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 4 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_4[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_4[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 5 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_5[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_5[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 6 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_6[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_6[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 				
 				/* 7 direction */
 				sum = 0;
 				for(int a=-1; a<2; a++){
 					for(int b=-1; b<2; b++){
-						sum = sum + this.image_input.getPixel(i+a, j+b) * mask_7[a+1][b+1];
+						sum = sum + image_input.getPixel(i+a, j+b) * mask_7[a+1][b+1];
 					}
 				}
 				
 				if(sum > max){sum = max;}
 				if(sum < 0){sum = 0;}	
-				if(sum > this.image_input.getPixel(i, j)){
-					this.image_input.setPixel(i, j, sum);
+				if(sum > image_input.getPixel(i, j)){
+					image_input.setPixel(i, j, sum);
 				}
 			}
 		}
 		
 		if(threshold == 1){
-			for(int i=0; i<this.image_input.getWidth(); i++){
-				for(int j=0; j<this.image_input.getHeight(); j++){
-					if(this.image_input.getPixel(i, j) > high){
-						this.image_input.setPixel(i, j, new_hi);
+			for(int i=0; i<image_input.getWidth(); i++){
+				for(int j=0; j<image_input.getHeight(); j++){
+					if(image_input.getPixel(i, j) > high){
+						image_input.setPixel(i, j, new_hi);
 						}
 					else{
-						this.image_input.setPixel(i, j, new_low);
+						image_input.setPixel(i, j, new_low);
 						}
 					}
 				}
@@ -310,16 +321,16 @@ public class Cartdroonize {
 		 * @version 0.20130821
 		 * @param posterization_strength: The strength of the posterization effect.
 		 */
-		for(int i = 0; i < this.image_input.getWidth(); i++){
-			for(int j = 0;j < this.image_input.getHeight(); j++){  
-				int pixel = this.image_input.getPixel(i, j);
+		for(int i = 0; i < image_input.getWidth(); i++){
+			for(int j = 0;j < image_input.getHeight(); j++){  
+				int pixel = image_input.getPixel(i, j);
 				int red_current = Color.red(pixel);
 				int green_current = Color.green(pixel);
 				int blue_current = Color.blue(pixel);
 				int red_new = (red_current - (red_current % posterization_strength));
 				int green_new = (green_current - (green_current % posterization_strength));
 				int blue_new = (blue_current - (blue_current % posterization_strength));
-                this.image_input.setPixel(i, j, Color.rgb(red_new, green_new, blue_new));
+                image_input.setPixel(i, j, Color.rgb(red_new, green_new, blue_new));
 			}
 		}
 	}
